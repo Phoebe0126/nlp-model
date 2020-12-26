f0f9997d7f673f2f9498e5ccf8e69847e64c30f5
Phoebe0126 <1542756558@qq.com>
2020-12-26 13:44:16 +0800

    add transformer

---
+++ transformer/transformer.py
@@ -0,0 +1,338 @@
+import copy
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import math
+import torch.optim as optim
+import torch.utils.data as Data
+
+
+def clones(module, N):
+    """拷贝生成N个layer"""
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+
+class Encoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+
+
+class LayerNorm(nn.Module):
+    """对于每个样本进行归一化"""
+
+    def __init__(self, features, eps=1e-6):
+        """features: 维度统一为dmodel,即512"""
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        # 均值
+        mean = x.mean(-1, keepdim=True)
+        # 标准差
+        std = x.std(-1, keepdim=True)
+        # 公式：norm(x) = a * (x - mean) / (std + eps) + b
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+
+class SublayerConnection(nn.Module):
+    "子层， 一般encoder和decoder分别由2、3个子层组成"
+
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        # Residual Connect + Multi-Head， 这里的norm是上一步的norm
+        return x + self.dropout(sublayer(self.norm(x)))
+
+
+class EncoderLayer(nn.Module):
+    """整个Encoder有多个self-attn和FFN组成"""
+
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
+        return self.sublayer[1](x, self.feed_forward)
+
+
+class Decoder(nn.Module):
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
+
+
+class DecoderLayer(nn.Module):
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayer, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
+
+def subsequent_mask(size):
+    attn_shape = (1, size, size)
+    # 对角线左下部分全为0，包括对角线
+    subsequent_mask = np.triu(np.ones(attn_shape), k=1).astype('uint8')
+    # 对角线左下为True, 其余为False, 表示可以attend的范围
+    return torch.from_numpy(subsequent_mask) == 0
+
+
+def attention(query, key, value, mask=None, dropout=None):
+    """计算点积，公式..."""
+    d_k = query.size(-1)
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask is not None:
+        # 将attend到后面的部分赋值为负无穷
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = F.softmax(scores, dim=-1)
+    if dropout is not None:
+        p_attn = dropout(p_attn)
+    return torch.matmul(p_attn, value), p_attn
+
+
+class MultiHeadedAttention(nn.Module):
+    def __init__(self, h, d_model, dropout=0.1):
+        """
+        :param h: head的个数，假设为8，则d_k为64
+        :param d_model: 512
+        :param dropout:
+        """
+        super(MultiHeadedAttention, self).__init__()
+        assert d_model % h == 0
+        self.d_k = d_model // h
+        self.h = h
+        self.linears = clones(nn.Linear(d_model, d_model), 4)
+        self.attn = None
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, query, key, value, mask=None):
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+        # query = (batch_size, seq_len, d_model)
+        nbatches = query.size(0)
+
+        # 使用线性变换将query、key、value划分为h个，即d_model -> d_k * h
+        query, key, value = [l(x).view(nbatches, -1, self.h, self.d_k).transpose(1, 2)
+                             for l, x in zip(self.linears, (query, key, value))]
+
+        x, self.attn = attention(query, key, value, mask=mask, dropout=self.dropout)
+
+        # 将h个head的attention结果拼接起来， 因为用到transpose为了不影响之前的值，需要加contiguous
+        x = x.transpose(1, 2).contiguous().view(nbatches, -1, self.h * self.d_k)
+        # 再做一次线性变换
+        return self.linears[-1](x)
+
+
+class PositionwiseFeedForward(nn.Module):
+    # FFN(x) = relu(xW1 + b1)W2 + b2
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+
+
+class Embeddings(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Embeddings, self).__init__()
+        self.lut = nn.Embedding(vocab, d_model)
+        self.d_model = d_model
+
+    def forward(self, x):
+        return self.lut(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x += self.pe[:, :x.size(1)]
+        return self.dropout(x)
+
+
+class EncoderDecoder(nn.Module):
+    def __init__(self, encoder, decoder, src_embed, tgt_embed, generator):
+        super(EncoderDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.generator = generator
+
+    def forward(self, src, tgt, src_mask, tgt_mask):
+        return self.decode(self.encode(src, src_mask), src_mask, tgt, tgt_mask)
+
+    def encode(self, src, src_mask):
+        return self.encoder(self.src_embed(src), src_mask)
+
+    def decode(self, memory, src_mask, tgt, tgt_mask):
+        return self.decoder(self.tgt_embed(tgt), memory, src_mask, tgt_mask)
+
+
+class Generator(nn.Module):
+    def __init__(self, d_model, vocab):
+        super(Generator, self).__init__()
+        self.proj = nn.Linear(d_model, vocab)
+
+    def forward(self, x):
+        return F.log_softmax(self.proj(x), dim=-1)
+
+
+def make_model(src_vocab, tgt_vocab, N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
+    c = copy.deepcopy
+    attn = MultiHeadedAttention(h, d_model)
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+    position = PositionalEncoding(d_model, dropout)
+    model = EncoderDecoder(Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
+                           Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
+                           nn.Sequential(Embeddings(d_model, src_vocab), c(position)),
+                           nn.Sequential(Embeddings(d_model, tgt_vocab), c(position)),
+                           Generator(d_model, tgt_vocab))
+
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform(p)
+    return model
+
+
+# S: Symbol that shows starting of decoding input
+# E: Symbol that shows starting of decoding output
+# P: Symbol that will fill in blank sequence if current batch data size is short than time steps
+sentences = [
+    # enc_input                dec_input            dec_output
+    ['ich mochte ein bier P', 'S i want a beer .', 'i want a beer . E'],
+    ['ich mochte ein cola P', 'S i want a coke .', 'i want a coke . E']
+]
+
+# Padding Should be Zero
+src_vocab = {'P': 0, 'ich': 1, 'mochte': 2, 'ein': 3, 'bier': 4, 'cola': 5}
+src_vocab_size = len(src_vocab)
+
+tgt_vocab = {'P': 0, 'i': 1, 'want': 2, 'a': 3, 'beer': 4, 'coke': 5, 'S': 6, 'E': 7, '.': 8}
+idx2word = {i: w for i, w in enumerate(tgt_vocab)}
+tgt_vocab_size = len(tgt_vocab)
+
+src_len = 5  # enc_input max sequence length
+tgt_len = 6  # dec_input(=dec_output) max sequence length
+
+
+def make_data(sentences):
+    enc_inputs, dec_inputs, dec_outputs = [], [], []
+    for i in range(len(sentences)):
+        enc_input = [[src_vocab[n] for n in sentences[i][0].split()]]  # [[1, 2, 3, 4, 0], [1, 2, 3, 5, 0]]
+        dec_input = [[tgt_vocab[n] for n in sentences[i][1].split()]]  # [[6, 1, 2, 3, 4, 8], [6, 1, 2, 3, 5, 8]]
+        dec_output = [[tgt_vocab[n] for n in sentences[i][2].split()]]  # [[1, 2, 3, 4, 8, 7], [1, 2, 3, 5, 8, 7]]
+
+        enc_inputs.extend(enc_input)
+        dec_inputs.extend(dec_input)
+        dec_outputs.extend(dec_output)
+
+    return torch.LongTensor(enc_inputs), torch.LongTensor(dec_inputs), torch.LongTensor(dec_outputs)
+
+
+enc_inputs, dec_inputs, dec_outputs = make_data(sentences)
+
+
+class MyDataSet(Data.Dataset):
+    def __init__(self, enc_inputs, dec_inputs, dec_outputs):
+        super(MyDataSet, self).__init__()
+        self.enc_inputs = enc_inputs
+        self.dec_inputs = dec_inputs
+        self.dec_outputs = dec_outputs
+
+    def __len__(self):
+        return self.enc_inputs.shape[0]
+
+    def __getitem__(self, idx):
+        return self.enc_inputs[idx], self.dec_inputs[idx], self.dec_outputs[idx]
+
+
+loader = Data.DataLoader(MyDataSet(enc_inputs, dec_inputs, dec_outputs), 1, True)
+
+model = make_model(src_vocab_size, tgt_vocab_size)
+criterion = nn.CrossEntropyLoss(ignore_index=0)
+optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.99)
+
+model.train()
+for epoch in range(30):
+    for enc_inputs, dec_inputs, dec_outputs in loader:
+        '''
+      enc_inputs: [batch_size, src_len]
+      dec_inputs: [batch_size, tgt_len]
+      dec_outputs: [batch_size, tgt_len]
+      '''
+        # enc_inputs, dec_inputs, dec_outputs = enc_inputs.to(device), dec_inputs.to(device), dec_outputs.to(device)
+        # outputs: [batch_size * tgt_len, tgt_vocab_size]
+        outputs = model(enc_inputs, dec_inputs, None, subsequent_mask(dec_inputs.size(1)))
+        loss = criterion(outputs.view(-1, 512), dec_outputs.view(-1))
+        print('Epoch:', '%04d' % (epoch + 1), 'loss =', '{:.6f}'.format(loss))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type_as(src.data)
+    for i in range(max_len-1):
+        out = model.decode(memory, src_mask,
+                           ys,
+                           subsequent_mask(ys.size(1).type_as(src.data)))
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.data[0]
+        ys = torch.cat([ys,
+                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+    return ys
+
+
+# test
+model.eval()
+for enc_inputs, dec_inputs, dec_outputs in loader:
+    print(greedy_decode(model, enc_inputs, None, len(enc_inputs), 6))
+
